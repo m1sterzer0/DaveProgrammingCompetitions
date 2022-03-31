f29c2468cf770321f6669206af8b75fe2e0f8294
m1sterzer0 <debr5836@gmail.com>
2022-03-31 04:33:22 -0600

    finishing kickstart 2018_G

--- kickstart/go/2018_G/CaveEsacpe/CaveEsacpe.go
+++ kickstart/go/2018_G/CaveEsacpe/CaveEsacpe.go
@@ -1,8 +1,8 @@
-
 package main
+
 import (
 	"bufio"
-    "fmt"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -10,46 +10,10 @@ var wrtr = bufio.NewWriterSize(os.Stdout, 10000000)
 var rdr = bufio.NewScanner(os.Stdin)
 func gs() string  { rdr.Scan(); return rdr.Text() }
 func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
-func gi2() (int,int) { return gi(),gi() }
-func gi3() (int,int,int) { return gi(),gi(),gi() }
-func gi4() (int,int,int,int) { return gi(),gi(),gi(),gi() }
-func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
-func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
-func gbs() []byte { return []byte(gs()) }
-func gfs(n int) []float64  { res := make([]float64,n); for i:=0;i<n;i++ { res[i] = gf() }; return res }
-func gss(n int) []string  { res := make([]string,n); for i:=0;i<n;i++ { res[i] = gs() }; return res }
-func ia(m int) []int { return make([]int,m) }
-func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
 func twodi(n int,m int,v int) [][]int {
 	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
 }
-func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
-func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
-func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
-func abs(a int) int { if a < 0 { return -a }; return a }
-func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
-func max(a,b int) int { if a > b { return a }; return b }
-func min(a,b int) int { if a > b { return b }; return a }
-func tern(cond bool, a int, b int) int { if cond { return a }; return b }
-func terns(cond bool, a string, b string) string { if cond { return a }; return b }
-func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
-func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
-func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
-func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
-func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
-func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
-func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
-func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
-func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
-func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
-func makefact(n int,mod int) ([]int,[]int) {
-	fact,factinv := make([]int,n+1),make([]int,n+1)
-	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
-	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
-	return fact,factinv
-}
-const inf int = 2000000000000000000
-const MOD int = 1000000007
+type node struct {x,y int}
 func main() {
 	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
 	defer wrtr.Flush()
@@ -57,9 +21,68 @@ func main() {
 	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
 	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
     T := gi()
+
     for tt:=1;tt<=T;tt++ {
 	    // PROGRAM STARTS HERE
-        fmt.Fprintf(wrtr,"Case #%v: %v\n",tt,0)
+		N,M,E,SR,SC,TR,TC := gi(),gi(),gi(),gi(),gi(),gi(),gi()
+		SR--; SC--; TR--; TC--
+		V := twodi(N,M,0)
+		for i:=0;i<N;i++ { for j:=0;j<M;j++ { V[i][j] = gi() } }
+		sb := twodi(N,M,-1); nodenum := 0;  values := make([]int,10000); dxy := []node{{-1,0},{1,0},{0,-1},{0,1}}
+		floodfill := func(x,y int) {
+			q := make([]node,0)
+			sb[x][y] = nodenum
+			q = append(q,node{x,y})
+			for len(q) > 0 {
+				xx := q[0].x; yy := q[0].y; q = q[1:]; values[nodenum] += V[xx][yy]
+				for _,ddxy := range dxy {
+					xxx,yyy := xx+ddxy.x,yy+ddxy.y; if xxx >= 0 && xxx < N && yyy >= 0 && yyy < M && V[xxx][yyy] >= 0 && sb[xxx][yyy] == -1 { sb[xxx][yyy] = nodenum; q = append(q,node{xxx,yyy}) }
+				}
+			}
+		}
+		traps := make([]node,0)
+		for i:=0;i<N;i++ {
+			for j:=0;j<M;j++ {
+				if sb[i][j] == -1 && V[i][j] >= 0 { floodfill(i,j); nodenum++ }
+				if V[i][j] <= -1 && V[i][j] >= -99999 { traps = append(traps, node{i,j})}
+			}
+		}
+		best := -1; analyzed := make([]bool,1<<uint(len(traps)))
+		var analyzeTrapCombo func(tmask int)
+		analyzeTrapCombo = func(tmask int) {
+			analyzed[tmask] = true
+			sid := sb[SR][SC]; eid := sb[TR][TC]
+			visited := make(map[int]bool)
+			visitedTraps := make(map[node]bool)
+			score := E + values[sid]; visited[sid] = true
+			for i:=0;i<len(traps);i++ {
+				if (tmask >> uint(i)) & 1 == 0 { continue }
+				tx,ty := traps[i].x,traps[i].y
+				visitedTraps[node{tx,ty}] = true 
+				score += V[tx][ty]
+				for _,ddxy := range dxy {
+					nx,ny := tx+ddxy.x,ty+ddxy.y
+					if nx < 0 || nx >= N || ny < 0 || ny >= M { continue }
+					nid := sb[nx][ny]
+					if nid >= 0 && !visited[nid] { visited[nid] = true; score += values[nid]}
+				}
+			}
+			if visited[eid] && best < score { best = score }
+			for i:=0;i<len(traps);i++ {
+				if analyzed[tmask | (1<<uint(i))] { continue } 
+				tx,ty := traps[i].x,traps[i].y
+				if score + V[tx][ty] < 0 { continue }
+				found := false
+				for _,ddxy := range(dxy) {
+					nx,ny := tx+ddxy.x,ty+ddxy.y
+					if nx < 0 || nx >= N || ny < 0 || ny >= M { continue }
+					if visited[sb[nx][ny]] || visitedTraps[node{nx,ny}] { found = true; break }
+				}
+				if found { analyzeTrapCombo(tmask | (1 << uint(i))) }
+			}
+		}
+		analyzeTrapCombo(0)
+        fmt.Fprintf(wrtr,"Case #%v: %v\n",tt,best)
     }
 }
 
