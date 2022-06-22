19fdbb4201885fa1787456e4c1d7e013aa4700ea
m1sterzer0 <debr5836@gmail.com>
2022-06-22 12:30:43 -0600

    codeforces global round 3

--- codeforces/go/gocfconfig.py
+++ codeforces/go/gocfconfig.py
@@ -6,24 +6,25 @@ import shutil
 def mkGoStarterFile(fn) :
     ttt = '''
 package main
+
 import (
 	"bufio"
 	"os"
 	"strconv"
-    "strings"
+	"strings"
 )
 var wrtr = bufio.NewWriterSize(os.Stdout, 10_000_000)
 var rdr = bufio.NewScanner(os.Stdin)
 func gs() string  { rdr.Scan(); return rdr.Text() }
 func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
-func gi2() (int,int) { return gi(),gi() }
-func gi3() (int,int,int) { return gi(),gi(),gi() }
-func gi4() (int,int,int,int) { return gi(),gi(),gi(),gi() }
 func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
 func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
 func gbs() []byte { return []byte(gs()) }
 func gfs(n int) []float64  { res := make([]float64,n); for i:=0;i<n;i++ { res[i] = gf() }; return res }
 func gss(n int) []string  { res := make([]string,n); for i:=0;i<n;i++ { res[i] = gs() }; return res }
+func gi64() int64     { i,e := strconv.ParseInt(gs(),10,64); if e != nil {panic(e)}; return i }
+func gis64(n int) []int64  { res := make([]int64,n); for i:=0;i<n;i++ { res[i] = gi64() }; return res }
+
 func ia(m int) []int { return make([]int,m) }
 func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
 func twodi(n int,m int,v int) [][]int {
@@ -36,8 +37,6 @@ func abs(a int) int { if a < 0 { return -a }; return a }
 func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
 func max(a,b int) int { if a > b { return a }; return b }
 func min(a,b int) int { if a > b { return b }; return a }
-func tern(cond bool, a int, b int) int { if cond { return a }; return b }
-func terns(cond bool, a string, b string) string { if cond { return a }; return b }
 func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
 func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
 func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
@@ -47,13 +46,42 @@ func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res *
 func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
 func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
 func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
 func makefact(n int,mod int) ([]int,[]int) {
 	fact,factinv := make([]int,n+1),make([]int,n+1)
 	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
 	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
 	return fact,factinv
 }
-func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+
+func ia64(m int) []int64 { return make([]int64,m) }
+func iai64(m int,v int64) []int64 { a := make([]int64,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi64(n int,m int,v int64) [][]int64 {
+	r := make([][]int64,n); for i:=0;i<n;i++ { x := make([]int64,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill264(m int) ([]int64,[]int64) { a,b := ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i] = gi64(),gi64()}; return a,b }
+func fill364(m int) ([]int64,[]int64,[]int64) { a,b,c := ia64(m),ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi64(),gi64(),gi64()}; return a,b,c }
+func fill464(m int) ([]int64,[]int64,[]int64,[]int64) { a,b,c,d := ia64(m),ia64(m),ia64(m),ia64(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi64(),gi64(),gi64(),gi64()}; return a,b,c,d }
+func abs64(a int64) int64 { if a < 0 { return -a }; return a }
+func rev64(a []int64) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max64(a,b int64) int64 { if a > b { return a }; return b }
+func min64(a,b int64) int64 { if a > b { return b }; return a }
+func maxarr64(a []int64) int64 { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr64(a []int64) int64 { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr64(a []int64) int64 { ans := int64(0); for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr64(a []int64) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod64(a,e,mod int64) int64 { res, m := int64(1), a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint64(a,e int64) int64 { res, m := int64(1), a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd64(a,b int64) int64 { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended64(a,b int64) (int64,int64,int64) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended64(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv64(a,m int64) (int64,bool) { g,x,_ := gcdExtended64(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecint64string(a []int64) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.FormatInt(a,10) }; return strings.Join(astr," ") }
+func makefact64(n int,mod int64) ([]int64,[]int64) {
+	fact,factinv := make([]int64,n+1),make([]int64,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * int64(i) % mod }
+	factinv[n] = powmod64(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * int64(i+1) % mod }
+	return fact,factinv
+}
 func main() {
 	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
 	defer wrtr.Flush()
@@ -123,8 +151,8 @@ if __name__ == "__main__" :
     probList += [(f"r800",  f"r800_{x}")   for x in ("A","B","C","D","E")]
 
     probList += [(f"gr1",  f"gr1_{x}")   for x in ("A","B","C","D","E","F")]
-    #probList += [(f"gr2",  f"gr2_{x}")   for x in ("A","B","C","D","E")]
-    #probList += [(f"gr3",  f"gr3_{x}")   for x in ("A","B","C","D","E","F")]
+    probList += [(f"gr2",  f"gr2_{x}")   for x in ("A","B","C","D","E")]
+    probList += [(f"gr3",  f"gr3_{x}")   for x in ("A","B","C","D","E","F")]
     #probList += [(f"gr4",  f"gr4_{x}")   for x in ("A","B","C","D","E","F1","F2")]
     #probList += [(f"gr5",  f"gr5_{x}")   for x in ("A","B","C","D","E","F")]
     #probList += [(f"gr6",  f"gr6_{x}")   for x in ("A","B","C","D","E","F")]
