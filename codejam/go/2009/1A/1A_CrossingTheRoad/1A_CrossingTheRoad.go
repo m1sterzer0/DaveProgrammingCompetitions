7d6b331de07ae16fa915db31996de78d2085d4f5
m1sterzer0 <debr5836@gmail.com>
2022-07-23 11:07:10 -0600

    codejam 2009 1A golang

---
+++ codejam/go/2009/1A/1A_CrossingTheRoad/1A_CrossingTheRoad.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+var wrtr = bufio.NewWriterSize(os.Stdout, 10000000)
+var rdr = bufio.NewScanner(os.Stdin)
+
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
+func gbs() []byte { return []byte(gs()) }
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
+}
+func sortUniq(a []int) []int {
+    sort.Slice(a,func(i,j int) bool { return a[i] < a[j] } )
+    n,j := len(a),0; if n == 0 { return a }
+    for i:=0;i<n;i++ { if a[i] != a[j] { j++; a[j] = a[i] } }; return a[:j+1]
+}
+
+type dnode struct { d,i,j int }
+type minheap struct { buf []dnode; less func(dnode, dnode) bool }
+func Newminheap(f func(dnode, dnode) bool) *minheap { buf := make([]dnode, 0); return &minheap{buf, f} }
+func (q *minheap) IsEmpty() bool { return len(q.buf) == 0 }
+func (q *minheap) Clear() { q.buf = q.buf[:0] }
+func (q *minheap) Len() int { return len(q.buf) }
+func (q *minheap) Push(v dnode) { q.buf = append(q.buf, v); q.siftdown(0, len(q.buf)-1) }
+func (q *minheap) Head() dnode { return q.buf[0] }
+func (q *minheap) Pop() dnode {
+	v1 := q.buf[0]; l := len(q.buf)
+	if l == 1 { q.buf = q.buf[:0] } else { l--; q.buf[0] = q.buf[l]; q.buf = q.buf[:l]; q.siftup(0) }; return v1
+}
+func (q *minheap) Heapify(pri []dnode) {
+	q.buf = append(q.buf, pri...); n := len(q.buf); for i := n/2 - 1; i >= 0; i-- { q.siftup(i) }
+}
+func (q *minheap) siftdown(startpos, pos int) {
+	newitem := q.buf[pos]
+	for pos > startpos {
+		ppos := (pos - 1) >> 1; p := q.buf[ppos]; if !q.less(newitem, p) { break }; q.buf[pos], pos = p, ppos
+	}
+	q.buf[pos] = newitem
+}
+func (q *minheap) siftup(pos int) {
+	endpos, startpos, newitem, chpos := len(q.buf), pos, q.buf[pos], 2*pos+1
+	for chpos < endpos {
+		rtpos := chpos + 1; if rtpos < endpos && !q.less(q.buf[chpos], q.buf[rtpos]) { chpos = rtpos }
+		q.buf[pos], pos = q.buf[chpos], chpos; chpos = 2*pos + 1
+	}
+	q.buf[pos] = newitem; q.siftdown(startpos, pos)
+}
+
+func main() {
+	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
+	defer wrtr.Flush(); infn := ""; if len(os.Args) > 1 { infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
+	// PROGRAM STARTS HERE
+    T := gi()
+    for tt:=1;tt<=T;tt++ {
+		N,M := gi(),gi()
+		S,W,T := twodi(N,M,0),twodi(N,M,0),twodi(N,M,0)
+		for i:=0;i<N;i++ { for j:=0;j<M;j++ { S[i][j] = gi(); W[i][j] = gi(); T[i][j] = gi() } }
+		inf := 1 << 62
+		darr := twodi(2*N,2*M,inf)
+		mh := Newminheap(func(a,b dnode) bool { return a.d < b.d })
+		mh.Push(dnode{0,2*N-1,0})
+		for !mh.IsEmpty() {
+			z := mh.Pop(); d,i,j := z.d,z.i,z.j
+			if darr[i][j] != inf { continue }
+			darr[i][j] = d
+			s,w,t := S[i/2][j/2],W[i/2][j/2],T[i/2][j/2]
+			t = t % (s+w) - (s+w)
+			nstime := d+1; if (d-t) % (s+w) >= s { nstime = t + (d-t)/(s+w)*(s+w)+(s+w)+1 }
+			ewtime := d+1; if (d-t) % (s+w) < s  { ewtime = t + (d-t)/(s+w)*(s+w)+s+1 }
+			if i & 1 == 0 {
+				if i-1 >= 0 { mh.Push(dnode{d+2,i-1,j}) }
+				mh.Push(dnode{nstime,i+1,j})
+			} else {
+				mh.Push(dnode{nstime,i-1,j})
+				if i+1 < 2*N { mh.Push(dnode{d+2,i+1,j}) }
+			}
+			if j & 1 == 0 {
+				if j-1 >= 0 { mh.Push(dnode{d+2,i,j-1}) }
+				mh.Push(dnode{ewtime,i,j+1})
+			} else {
+				mh.Push(dnode{ewtime,i,j-1})
+				if j+1 < 2*M { mh.Push(dnode{d+2,i,j+1}) }
+			}
+		}
+        fmt.Fprintf(wrtr,"Case #%v: %v\n",tt,darr[0][2*M-1])
+    }
+}
+
