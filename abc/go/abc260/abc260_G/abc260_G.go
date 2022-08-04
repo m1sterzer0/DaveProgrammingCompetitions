fa13d6f72cf9bd8b81a48e1644fc6a230fc73cf7
m1sterzer0 <debr5836@gmail.com>
2022-08-03 21:13:44 -0600

    abc260 golang

---
+++ abc/go/abc260/abc260_G/abc260_G.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+var wrtr = bufio.NewWriterSize(os.Stdout, 10_000_000)
+var rdr = bufio.NewScanner(os.Stdin)
+
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
+func gbs() []byte { return []byte(gs()) }
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
+}
+func sortUniq(a []int) []int {
+    sort.Slice(a,func(i,j int) bool { return a[i] < a[j] } )
+    n,j := len(a),0; if n == 0 { return a }
+    for i:=0;i<n;i++ { if a[i] != a[j] { j++; a[j] = a[i] } }; return a[:j+1]
+}
+
+func main() {
+	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
+	defer wrtr.Flush(); infn := ""; if len(os.Args) > 1 { infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
+	// PROGRAM STARTS HERE
+	N,M := gi(),gi(); bd := make([]string,N); for i:=0;i<N;i++ { bd[i] = gs(); }
+	Q := gi(); X,Y := fill2(Q); for i:=0;i<Q;i++ { X[i]--; Y[i]-- }
+	pdel2,ndel2 := twodi(N,N,0),twodi(N,N,0)
+	for i:=0;i<N;i++ {
+		for j:=0;j<N;j++ {
+			if bd[i][j] == 'X' { continue }
+			pdel2[i][j]++
+			if i+M < N { pdel2[i+M][j]-- }
+			if j+2*M < N {
+				ndel2[i][j+2*M] += 1
+			} else {
+				rowdel := (j+2*M-(N-2))/2
+				col := j+2*M-2*rowdel
+				if i+rowdel < N { ndel2[i+rowdel][col]++}
+			}
+			if i+M < N { ndel2[i+M][j] -= 1}
+		}
+	}
+	del1:= twodi(N,N,0)
+	for j:=0;j<N;j++ {
+		running := 0
+		for i:=0;i<N;i++ { running += pdel2[i][j]; del1[i][j] = running }
+	}
+	for j:=0;j<N-2;j++ {
+		ii,jj,running := 0,j,0
+		for (jj >= 0) { running += ndel2[ii][jj]; del1[ii][jj] -= running; ii++; jj-=2 }
+	}
+	for i:=0;i<N;i++ {
+		for j:=N-2;j<N;j++ {
+			ii,jj,running := i,j,0
+			for (ii<N && jj>=0) { running += ndel2[ii][jj]; del1[ii][jj] -= running; ii++; jj-=2 }
+		}
+	}
+	ansarr := twodi(N,N,0)
+	for i:=0;i<N;i++ {
+		running := 0
+		for j:=0;j<N;j++ { running += del1[i][j]; ansarr[i][j] = running; }
+	}
+	for i:=0;i<Q;i++ { fmt.Fprintln(wrtr,ansarr[X[i]][Y[i]]) }
+}
+
