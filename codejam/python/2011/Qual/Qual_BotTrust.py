716ec3730715bc1a8227ac590e4d6d981b8f422d
m1sterzer0 <debr5836@gmail.com>
2022-08-12 13:32:28 -0600

    codejam 2011 Qual python

---
+++ codejam/python/2011/Qual/Qual_BotTrust.py
@@ -0,0 +1,43 @@
+import sys
+from collections import deque
+
+## Input crap
+infile = sys.stdin
+intokens = deque()
+def getTokens(): 
+    while not intokens:
+        for c in infile.readline().rstrip().split() :
+            intokens.append(c)    
+def gs(): getTokens(); return intokens.popleft()
+def gi(): return int(gs())
+def gf(): return float(gs())
+def gbs(): return [c for c in gs()]
+def gis(n): return [gi() for i in range(n)]
+def ia(m): return [0] * m
+def iai(m,v): return [v] * m
+def twodi(n,m,v): return [iai(m,v) for i in range(n)]
+def fill2(m) : r = gis(2*m); return r[0::2],r[1::2]
+def fill3(m) : r = gis(3*m); return r[0::3],r[1::3],r[2::3]
+def fill4(m) : r = gis(4*m); return r[0::4],r[1::4],r[2::4],r[3::4]
+MOD = 998244353
+##MOD = 1000000007
+
+def main() :
+    if len(sys.argv) > 1 : global infile; infile = open(sys.argv[1],'rt')
+    ## PROGRAM STARTS HERE
+    T = gi()
+    for tt in range(1,T+1) :
+        opos,ot,bpos,bt = 1,0,1,0
+        n = gi()
+        for i in range(n) :
+            x,newpos = gs(),gi()
+            if x == "O" :
+                ot,opos = max(bt+1,ot+abs(newpos-opos)+1),newpos
+            else :
+                bt,bpos = max(ot+1,bt+abs(newpos-bpos)+1),newpos
+        ans = max(ot,bt)
+        print(f"Case #{tt}: {ans}")
+
+if __name__ == "__main__" :
+    main()
+
