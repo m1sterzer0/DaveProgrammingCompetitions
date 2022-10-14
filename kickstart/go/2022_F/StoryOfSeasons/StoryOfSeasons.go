ad4cdc183cc243e7dc53d0e9513f4f4562acab1a
m1sterzer0 <debr5836@gmail.com>
2022-10-14 13:57:55 -0600

    kickstart 2022_F

---
+++ kickstart/go/2022_F/StoryOfSeasons/StoryOfSeasons.go
@@ -0,0 +1,118 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+var wrtr = bufio.NewWriterSize(os.Stdout, 10000000)
+var rdr = bufio.NewScanner(os.Stdin)
+
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
+func gbs() []byte { return []byte(gs()) }
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
+}
+func sortUniq(a []int) []int {
+    sort.Slice(a,func(i,j int) bool { return a[i] < a[j] } )
+    n,j := len(a),0; if n == 0 { return a }
+    for i:=0;i<n;i++ { if a[i] != a[j] { j++; a[j] = a[i] } }; return a[:j+1]
+}
+
+type crop struct {q,l,v int}
+type minheap struct { buf []crop; less func(crop, crop) bool }
+func Newminheap(f func(crop, crop) bool) *minheap { buf := make([]crop, 0); return &minheap{buf, f} }
+func (q *minheap) IsEmpty() bool { return len(q.buf) == 0 }
+func (q *minheap) Clear() { q.buf = q.buf[:0] }
+func (q *minheap) Len() int { return len(q.buf) }
+func (q *minheap) Push(v crop) { q.buf = append(q.buf, v); q.siftdown(0, len(q.buf)-1) }
+func (q *minheap) Head() crop { return q.buf[0] }
+func (q *minheap) Pop() crop {
+	v1 := q.buf[0]; l := len(q.buf)
+	if l == 1 { q.buf = q.buf[:0] } else { l--; q.buf[0] = q.buf[l]; q.buf = q.buf[:l]; q.siftup(0) }; return v1
+}
+func (q *minheap) Heapify(pri []crop) {
+	q.buf = append(q.buf, pri...); n := len(q.buf); for i := n/2 - 1; i >= 0; i-- { q.siftup(i) }
+}
+func (q *minheap) siftdown(startpos, pos int) {
+	newitem := q.buf[pos]
+	for pos > startpos {
+		ppos := (pos - 1) >> 1; p := q.buf[ppos]; if !q.less(newitem, p) { break }; q.buf[pos], pos = p, ppos
+	}
+	q.buf[pos] = newitem
+}
+func (q *minheap) siftup(pos int) {
+	endpos, startpos, newitem, chpos := len(q.buf), pos, q.buf[pos], 2*pos+1
+	for chpos < endpos {
+		rtpos := chpos + 1; if rtpos < endpos && !q.less(q.buf[chpos], q.buf[rtpos]) { chpos = rtpos }
+		q.buf[pos], pos = q.buf[chpos], chpos; chpos = 2*pos + 1
+	}
+	q.buf[pos] = newitem; q.siftdown(startpos, pos)
+}
+
+
+func main() {
+	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
+	defer wrtr.Flush(); infn := ""; if len(os.Args) > 1 { infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
+	// PROGRAM STARTS HERE
+    T := gi()
+    for tt:=1;tt<=T;tt++ {
+		D,N,X := gi(),gi(),gi(); Q,L,V := fill3(N)
+		c1 := make([]crop,N); for i:=0;i<N;i++ { c1[i] = crop{Q[i],L[i],V[i]} }
+		sort.Slice(c1,func(i,j int) bool { return c1[i].l < c1[j].l} )
+		mh := Newminheap(func(a,b crop) bool { return a.v > b.v })
+		// Get list of days -- do this the sloppy way
+		junk := make(map[int]bool); junk[0] = true; junk[1] = true; junk[D] = true
+		for _,c := range c1 { junk[D-c.l] = true }
+		days := make([]int,0,len(junk)); for k := range junk { days = append(days,k) }
+		sort.Slice(days,func(i,j int) bool { return days[i] > days[j] } )
+		inf := 1<<60; ans := 0; lastday := inf; idx := 0
+		for _,d := range days {
+			if lastday != inf { 
+				numToProcess := (lastday - (d+1)) * X
+				for numToProcess > 0 && !mh.IsEmpty() {
+					c := mh.Pop()
+					if c.q <= numToProcess { ans += c.v*c.q; numToProcess -= c.q; continue  }
+					ans += numToProcess*c.v; c.q -= numToProcess; numToProcess = 0; mh.Push(c)
+				}
+			}
+			for idx < N && d+c1[idx].l <= D { mh.Push(c1[idx]); idx++ } 
+			lastday = d+1
+		}
+        fmt.Fprintf(wrtr,"Case #%v: %v\n",tt,ans)
+    }
+}
+
