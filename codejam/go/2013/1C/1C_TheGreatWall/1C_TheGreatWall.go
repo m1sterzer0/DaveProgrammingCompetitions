27fad894e58f95f8260484f2764b03e43a9ab3f6
m1sterzer0 <debr5836@gmail.com>
2022-11-06 12:22:26 -0700

    codejam 2013 1C

---
+++ codejam/go/2013/1C/1C_TheGreatWall/1C_TheGreatWall.go
@@ -0,0 +1,191 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+var wrtr = bufio.NewWriterSize(os.Stdout, 10000000)
+var rdr = bufio.NewScanner(os.Stdin)
+
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
+func gbs() []byte { return []byte(gs()) }
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
+}
+func sortUniq(a []int) []int {
+    sort.Slice(a,func(i,j int) bool { return a[i] < a[j] } )
+    n,j := len(a),0; if n == 0 { return a }
+    for i:=0;i<n;i++ { if a[i] != a[j] { j++; a[j] = a[i] } }; return a[:j+1]
+}
+
+type lazysegtree struct {
+	n, size, log int; op func(int, int) int; mapping func(int, int) int
+	composition func(int, int) int; e int; id int; d []int; lz []int
+}
+func Newlazysegtree(n int, op func(int, int) int, mapping func(int, int) int, composition func(int, int) int, e int, id int) *lazysegtree {
+	v := make([]int, n); for i := 0; i < n; i++ { v[i] = e }
+	return NewlazysegtreeVec(v, op, mapping, composition, e, id)
+}
+func NewlazysegtreeVec(v []int, op func(int, int) int, mapping func(int, int) int, composition func(int, int) int, e int, id int) *lazysegtree {
+	n, sz, log := len(v), 1, 0; for sz < n { sz <<= 1; log += 1 }; d := make([]int, 2*sz)
+	lz := make([]int, sz); for i := 0; i < 2*sz; i++ { d[i] = e }; for i := 0; i < sz; i++ { lz[i] = id }; d[0] = e
+	for i := 0; i < n; i++ { d[sz+i] = v[i]; lz[i] = id }
+	st := &lazysegtree{n, sz, log, op, mapping, composition, e, id, d, lz}
+	for i := sz - 1; i >= 1; i-- { st.update(i) }; return st
+}
+func (q *lazysegtree) Set(p int, v int) {
+	p += q.size; for i := q.log; i >= 1; i-- { q.push(p >> uint(i)) }; q.d[p] = v
+	for i := 1; i <= q.log; i++ { q.update(p >> uint(i)) }
+}
+func (q *lazysegtree) Get(p int) int {
+	p += q.size; for i := q.log; i >= 1; i-- { q.push(p >> uint(i)) }; return q.d[p]
+}
+func (q *lazysegtree) Prod(l int, r int) int {
+	if r < l { return q.e }; l += q.size; r += q.size; r += 1 
+	for i := q.log; i >= 1; i-- {
+		if ((l >> uint(i)) << uint(i)) != l { q.push(l >> uint(i)) }
+		if ((r >> uint(i)) << uint(i)) != r { q.push((r - 1) >> uint(i)) }
+	}
+	sml, smr := q.e, q.e
+	for l < r {
+		if l&1 != 0 { sml = q.op(sml, q.d[l]); l++ }; if r&1 != 0 { r--; smr = q.op(q.d[r], smr) }; l >>= 1; r >>= 1
+	}
+	return q.op(sml, smr)
+}
+func (q *lazysegtree) Allprod() int { return q.d[1] }
+func (q *lazysegtree) Apply(p int, f int) {
+	p += q.size; for i := q.log; i >= 1; i-- { q.push(p >> uint(i)) }; q.d[p] = q.mapping(f, q.d[p])
+	for i := 1; i <= q.log; i++ { q.update(p >> uint(i)) }
+}
+func (q *lazysegtree) ApplyRange(l int, r int, f int) {
+	if r < l { return }; r += 1; l += q.size; r += q.size
+	for i := q.log; i >= 1; i-- {
+		if ((l >> uint(i)) << uint(i)) != l { q.push(l >> uint(i)) }
+		if ((r >> uint(i)) << uint(i)) != r { q.push((r - 1) >> uint(i)) }
+	}
+	l2, r2 := l, r
+	for l < r { if l&1 != 0 { q.allApply(l, f); l += 1 }; if r&1 != 0 { r -= 1; q.allApply(r, f) }; l >>= 1; r >>= 1 }
+	l, r = l2, r2
+	for i := 1; i <= q.log; i++ {
+		if ((l >> uint(i)) << uint(i)) != l { q.update(l >> uint(i)) }
+		if ((r >> uint(i)) << uint(i)) != r { q.update((r - 1) >> uint(i)) }
+	}
+}
+func (q *lazysegtree) MaxRight(l int, f func(int) bool) int {
+	if l == q.n { return q.n - 1 }; l += q.size; for i := q.log; i >= 1; i-- { q.push(l >> uint(i)) }; sm := q.e
+	for {
+		for l%2 == 0 { l >>= 1 }
+		if !f(q.op(sm, q.d[l])) {
+			for l < q.size { q.push(l); l *= 2; if f(q.op(sm, q.d[l])) { sm = q.op(sm, q.d[l]); l++ } }
+			return l - q.size - 1
+		}
+		sm = q.op(sm, q.d[l]); l++; if l&-l == l { break }
+	}
+	return q.n - 1
+}
+func (q *lazysegtree) MinLeft(r int, f func(int) bool) int {
+	if r < 0 { return 0 }; r += q.size; r++; for i := q.log; i >= 1; i-- { q.push((r - 1) >> uint(i)) }; sm := q.e 
+	for {
+		r--; for r > 1 && r%2 == 1 { r >>= 1 }
+		if !f(q.op(q.d[r], sm)) {
+			for r < q.size { q.push(r); r = 2*r + 1; if f(q.op(q.d[r], sm)) { sm = q.op(q.d[r], sm); r-- } }
+			return r + 1 - q.size
+		}
+		sm = q.op(q.d[r], sm); if r&-r == r { break }
+	}
+	return 0
+}
+func (q *lazysegtree) update(k int) { q.d[k] = q.op(q.d[2*k], q.d[2*k+1]) }
+func (q *lazysegtree) allApply(k int, f int) {
+	q.d[k] = q.mapping(f, q.d[k]); if k < q.size { q.lz[k] = q.composition(f, q.lz[k]) }
+}
+func (q *lazysegtree) push(k int) { q.allApply(2*k, q.lz[k]); q.allApply(2*k+1, q.lz[k]); q.lz[k] = q.id }
+
+func main() {
+	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
+	defer wrtr.Flush(); infn := ""; if len(os.Args) > 1 { infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
+	// PROGRAM STARTS HERE
+    T := gi()
+    for tt:=1;tt<=T;tt++ {
+		// lazy seg tree with coordinate compression??
+		N := gi()
+		D,NN,W,E,S,dd,dp,ds := ia(N),ia(N),ia(N),ia(N),ia(N),ia(N),ia(N),ia(N)
+		for i:=0;i<N;i++ { D[i],NN[i],W[i],E[i],S[i],dd[i],dp[i],ds[i] = gi(),gi(),gi(),gi(),gi(),gi(),gi(),gi() }
+		// Do the coordinate compression first
+		rawcoords := make([]int,0,2000*N)
+		for i:=0;i<N;i++ { for j:=0;j<NN[i];j++ { rawcoords = append(rawcoords,W[i]+dp[i]*j,E[i]+dp[i]*j) } }
+		uc := sortUniq(rawcoords)
+		// Need to deal with the space between the coordinates, so double things
+		cmap := make(map[int]int); for i,c := range uc { cmap[c] = 2*i }
+		// Set up the lazy seg tree
+		lstop := func(a,b int) int   { if a < b { return a }; return b }
+		lstcomp := func(f,g int) int { if f > g { return f};  return g }
+		lstmap := func(f,a int) int  { if f > a { return f }; return a }
+		v := iai(2*len(uc),0); lst := NewlazysegtreeVec(v,lstop,lstmap,lstcomp,1<<60,0)
+
+		// Create the list of events
+		type event struct { d,w,e,s int }
+		ee := make([]event,0,2000*N)
+		for i:=0;i<N;i++ { 
+			for j:=0;j<NN[i];j++ {
+				w := cmap[W[i]+dp[i]*j]
+				e := cmap[E[i]+dp[i]*j]-1
+				d := D[i] + j*dd[i]
+				s := S[i] + j*ds[i]
+				ee = append(ee,event{d,w,e,s})
+			}
+		}
+
+		// Sort and process the events
+		sort.Slice(ee,func(i,j int) bool { return ee[i].d < ee[j].d } )
+		ans := 0; idx := 0; nevents := len(ee)
+		for idx < nevents {
+			d := ee[idx].d
+			jidx := idx; for jidx+1 < nevents && ee[jidx+1].d == d { jidx++ }
+			for i:=idx;i<=jidx;i++ {
+				ss := lst.Prod(ee[i].w,ee[i].e)
+				if ss < ee[i].s  { ans++ }
+			}
+			for i:=idx;i<=jidx;i++ { 
+				lst.ApplyRange(ee[i].w,ee[i].e,ee[i].s)
+			}
+			idx = jidx+1
+		}
+        fmt.Printf("Case #%v: %v\n",tt,ans)
+    }
+}
+
