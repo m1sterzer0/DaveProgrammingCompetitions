943b9a1f287edff4c851a99929e6501555cfb688
m1sterzer0 <debr5836@gmail.com>
2023-01-02 11:53:41 -0700

    abc275

---
+++ abc/go/abc275/abc275_G/abc275_G.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+var wrtr = bufio.NewWriterSize(os.Stdout, 10_000_000)
+var rdr = bufio.NewScanner(os.Stdin)
+
+func gs() string  { rdr.Scan(); return rdr.Text() }
+func gi() int     { i,e := strconv.Atoi(gs()); if e != nil {panic(e)}; return i }
+func gis(n int) []int  { res := make([]int,n); for i:=0;i<n;i++ { res[i] = gi() }; return res }
+func gf() float64 { f,e := strconv.ParseFloat(gs(),64); if e != nil {panic(e)}; return f }
+func gbs() []byte { return []byte(gs()) }
+func ia(m int) []int { return make([]int,m) }
+func iai(m int,v int) []int { a := make([]int,m); for i:=0;i<m;i++ { a[i] = v }; return a }
+func twodi(n int,m int,v int) [][]int {
+	r := make([][]int,n); for i:=0;i<n;i++ { x := make([]int,m); for j:=0;j<m;j++ { x[j] = v }; r[i] = x }; return r
+}
+func fill2(m int) ([]int,[]int) { a,b := ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i] = gi(),gi()}; return a,b }
+func fill3(m int) ([]int,[]int,[]int) { a,b,c := ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i] = gi(),gi(),gi()}; return a,b,c }
+func fill4(m int) ([]int,[]int,[]int,[]int) { a,b,c,d := ia(m),ia(m),ia(m),ia(m); for i:=0;i<m;i++ {a[i],b[i],c[i],d[i] = gi(),gi(),gi(),gi()}; return a,b,c,d }
+func abs(a int) int { if a < 0 { return -a }; return a }
+func rev(a []int) { i,j := 0,len(a)-1; for i < j { a[i],a[j] = a[j],a[i]; i++; j-- } }
+func max(a,b int) int { if a > b { return a }; return b }
+func min(a,b int) int { if a > b { return b }; return a }
+func maxarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa > ans { ans = aa } }; return ans }
+func minarr(a []int) int { ans := a[0]; for _,aa := range(a) { if aa < ans { ans = aa } }; return ans }
+func sumarr(a []int) int { ans := 0; for _,aa := range(a) { ans += aa }; return ans }
+func zeroarr(a []int) { for i:=0; i<len(a); i++ { a[i] = 0 } }
+func powmod(a,e,mod int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m % mod }; m = m * m % mod; e >>= 1 }; return res }
+func powint(a,e int) int { res, m := 1, a; for e > 0 { if e&1 != 0 { res = res * m }; m = m * m; e >>= 1 }; return res }
+func gcd(a,b int) int { for b != 0 { t:=b; b=a%b; a=t }; return a }
+func gcdExtended(a,b int) (int,int,int) { if a == 0 { return b,0,1 }; gcd,x1,y1 := gcdExtended(b%a,a); return gcd, y1-(b/a)*x1,x1 }
+func modinv(a,m int) (int,bool) { g,x,_ := gcdExtended(a,m); if g != 1 { return 0,false }; return (x % m + m) % m,true  }
+func vecintstring(a []int) string { astr := make([]string,len(a)); for i,a := range a { astr[i] = strconv.Itoa(a) }; return strings.Join(astr," ") }
+func makefact(n int,mod int) ([]int,[]int) {
+	fact,factinv := make([]int,n+1),make([]int,n+1)
+	fact[0] = 1; for i:=1;i<=n;i++ { fact[i] = fact[i-1] * i % mod }
+	factinv[n] = powmod(fact[n],mod-2,mod); for i:=n-1;i>=0;i-- { factinv[i] = factinv[i+1] * (i+1) % mod }
+	return fact,factinv
+}
+func sortUniq(a []int) []int {
+    sort.Slice(a,func(i,j int) bool { return a[i] < a[j] } )
+    n,j := len(a),0; if n == 0 { return a }
+    for i:=0;i<n;i++ { if a[i] != a[j] { j++; a[j] = a[i] } }; return a[:j+1]
+}
+
+func main() {
+	//f1, _ := os.Create("cpu.prof"); pprof.StartCPUProfile(f1); defer pprof.StopCPUProfile()
+	defer wrtr.Flush(); infn := ""; if len(os.Args) > 1 { infn = os.Args[1] }
+	if infn != "" {	f, e := os.Open(infn); if e != nil { panic(e) }; rdr = bufio.NewScanner(f) }
+	rdr.Split(bufio.ScanWords); rdr.Buffer(make([]byte,1024),1000000000)
+	// PROGRAM STARTS HERE
+	N := gi()
+	A,B,C := fill3(N)
+	type wvcost struct { w,v float64 }
+	costs := make([]wvcost,N)
+	for i:=0;i<N;i++ { costs[i] = wvcost{float64(A[i])/float64(C[i]),float64(B[i])/float64(C[i]) } }
+	sort.Slice(costs, func(i,j int) bool { return costs[i].w < costs[j].w || costs[i].w == costs[j].w && costs[i].v < costs[j].v} )
+	llhull := make([]wvcost,0,N); n := 0
+	for _,c := range costs {
+		if n == 0 { llhull = append(llhull,c); n++; continue }
+		if llhull[n-1].v <= c.v { continue } // Don't want points strictly worse
+		for n >= 2 &&  llhull[n-2].v + (llhull[n-1].w-llhull[n-2].w)/(c.w-llhull[n-2].w)*(c.v-llhull[n-2].v) <= llhull[n-1].v { llhull = llhull[:n-1]; n--; }
+		llhull = append(llhull,c); n++
+	}
+	best := 1e99
+	for _,c := range llhull { cand1 := c.w; if c.v > c.w { cand1 = c.v }; if cand1 < best { best = cand1 } }
+	for i:=0;i<n-1;i++ {
+		if llhull[i].v > llhull[i].w && llhull[i+1].v < llhull[i+1].w { // Crossover point
+			// Need to solve intersection of 
+			//   y = w1 + (w2-w1)*x
+			//   y = v1 * (v2-v1)*x
+			// w1 + (w2-w1)*x = v1 * (v2-v1)*x
+			// (w1-v1) = (v2-v1-w2+w1)*x
+			w1,v1,w2,v2 := llhull[i].w,llhull[i].v,llhull[i+1].w,llhull[i+1].v
+			x := (w1-v1) / (v2-v1-w2+w1)
+			y := w1 + (w2-w1)*x
+			if y < best { best = y }
+		}
+	}
+	ans := 1.0/best
+	fmt.Println(ans)
+}
